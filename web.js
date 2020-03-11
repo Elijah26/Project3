baa4824575fbc908a0c8b9a7d7e7d039c625be0b
Astrocat2374 <sugiyama.evan@gmail.com>
2020-03-11 09:03:05 -1000

    third

---
+++ web.js
@@ -0,0 +1,16 @@
+function Price {
+	var Price = document.getElementById('urPrice').value;
+	var Cal = document.getElementById('CalForPrice').value;
+	var Add = document.getElementById('urAddress').value;
+	var total =document.getElementById('urTotal').value;
+}
+var p = 0.25;
+var s = 3;
+var e = 3;
+var b = 20;
+var d = 2;
+var total = p + s + e + b + d;
+function Add {
+	document.getElementById("urAddress").innerHTML = "Thank You for Shopping";
+}
+
